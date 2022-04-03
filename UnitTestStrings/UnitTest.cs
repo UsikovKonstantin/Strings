4ab4b0395125a2e0b1413f69e35703920d60e5a3
theponylover666 <dotaonelove332@gmail.com>
2022-04-03 23:27:33 +0300

    Расширил UnitTest'ы

--- UnitTestStrings/UnitTest.cs
+++ UnitTestStrings/UnitTest.cs
@@ -15,6 +15,13 @@ namespace UnitTestStrings
             Assert.AreEqual(Strings.IndexOfAny_Primitive(text, "Как", 0), 0);
             Assert.AreEqual(Strings.IndexOfAny_Primitive(text, "в наше время", 0), 87);
             Assert.AreEqual(Strings.IndexOfAny_Primitive(text, "когда", 0), 25);
+
+            string text2 = "Нынешний же пламенный юноша отскочил бы с ужасом, если бы показали ему его же портрет в старости. Забирайте же с собою в путь, выходя из мягких юношеских лет в суровое ожесточающее мужество, забирайте с собою все человеческие движения, не оставляйте их на дороге, не подымете потом! Грозна, страшна грядущая впереди старость, и ничего не отдает назад и обратно!";
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "Нынешний", 0), 0);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "Забирайте", 0), 98);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "с ужасом", 25), 40);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "забирайте", 99), 191);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "обратно", 0), 353);
         }
 
         [TestMethod]
@@ -25,6 +32,13 @@ namespace UnitTestStrings
             Assert.AreEqual(Strings.IndexOf_KMP(text, "Как", 0), 0);
             Assert.AreEqual(Strings.IndexOf_KMP(text, "в наше время", 0), 87);
             Assert.AreEqual(Strings.IndexOf_KMP(text, "когда", 0), 25);
+
+            string text2 = "Лениво дышит полдень мглистый, Лениво катится река - И в тверди пламенной и чистой. Лениво тают облака";
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "Лениво", 0), 0);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "полдень", 0), 13);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "река", 25), 46);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "Лениво", 70), 84);
+            Assert.AreEqual(Strings.IndexOfAny_Primitive(text2, "чистой", 0), 76);
         }
 
         [TestMethod]
@@ -36,6 +50,10 @@ namespace UnitTestStrings
             Assert.AreEqual(Strings.IndexOfKMP_Best(text, "можно", 0)[0], 4);
             Assert.AreEqual(Strings.IndexOfKMP_Best(text, "можно", 0)[1], 60);
 
+            string text2 = "Лениво дышит полдень мглистый, Лениво катится река - И в тверди пламенной и чистой. Лениво тают облака";
+            Assert.AreEqual(Strings.IndexOfKMP_Best(text2, "Лениво", 0)[1], 31);
+            Assert.AreEqual(Strings.IndexOfKMP_Best(text2, "Лениво", 0)[2], 84);
+            Assert.AreEqual(Strings.IndexOfKMP_Best(text2, "дышит", 0)[0], 7);
         }
 
         [TestMethod]
@@ -46,6 +64,13 @@ namespace UnitTestStrings
             Assert.IsTrue(Strings.IsPattern(text, "Как", 0));
             Assert.IsTrue(Strings.IsPattern(text, "в наше время", 87));
             Assert.IsTrue(Strings.IsPattern(text, "когда", 25));
+
+            string text2 = "Лениво дышит полдень мглистый, Лениво катится река - И в тверди пламенной и чистой. Лениво тают облака";
+            Assert.IsTrue(Strings.IsPattern(text2, "Лениво", 0));
+            Assert.IsTrue(Strings.IsPattern(text2, "полдень", 13));
+            Assert.IsTrue(Strings.IsPattern(text2, "река", 46));
+            Assert.IsTrue(Strings.IsPattern(text2, "Лениво", 84));
+            Assert.IsTrue(Strings.IsPattern(text2, "чистой", 76));
         }
 
         [TestMethod]
@@ -55,6 +80,12 @@ namespace UnitTestStrings
             Assert.AreEqual(Strings.PrefixFunction("abab")[1], 0);
             Assert.AreEqual(Strings.PrefixFunction("ababuiabc")[3], 2);
             Assert.AreEqual(Strings.PrefixFunction("ababuiabc")[7], 2);
+
+            Assert.AreEqual(Strings.PrefixFunction("абракадабра")[3], 1);
+            Assert.AreEqual(Strings.PrefixFunction("абракадабра")[4], 0);
+            Assert.AreEqual(Strings.PrefixFunction("абракадабра")[5], 1);
+            Assert.AreEqual(Strings.PrefixFunction("абракадабра")[8], 2);
+            Assert.AreEqual(Strings.PrefixFunction("абракадабра")[10], 4);
         }
     }
 }
